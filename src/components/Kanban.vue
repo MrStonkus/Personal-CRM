c45f4cbc81f1a215ccbe36fdd52a29799caf3961
Valdas Stonkus <mr.valdas.stonkus@gmail.com>
2019-03-17 00:35:59 +0200

    Added deals number in stage header

--- src/components/Kanban.vue
+++ src/components/Kanban.vue
@@ -2,6 +2,17 @@
   <div id="kanban">
     <!-- Kanban start   -->
     <kanban-board :stages="stages" :blocks="deals" @update-block="updateDeal">
+      <div
+        class="stages-header"
+        v-for="stage in stages"
+        :slot="stage"
+        :key="stage"
+      >
+        <div>
+          <h2>{{ stage }}</h2>
+        </div>
+        <div class="stage-value">{{ getDeals(stage) }} deals</div>
+      </div>
       <div v-for="deal in deals" :slot="deal.id" :key="deal.id">
         <div>
           <strong>id:</strong>
@@ -15,21 +26,20 @@
 </template>
 
 <script>
+import { mapState } from "vuex";
+
 export default {
   name: "KanBan",
   //Get data of deals from global Vuex store
   computed: {
-    stages() {
-      return this.$store.state.stages;
-    },
-    deals() {
-      return this.$store.state.deals;
-    }
+    ...mapState(["stages", "deals"])
   },
-  // data() {
-  //   return {};
-  // },
+
   methods: {
+    getDeals: function(stage) {
+      let deals = this.$store.state.deals;
+      return deals.filter(x => x.status === stage).length;
+    },
     updateDeal(id, status) {
       this.deals.find(d => d.id === Number(id)).status = status;
       console.log(this.deals);
