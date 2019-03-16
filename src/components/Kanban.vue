86026edb464ddd256b46438c59a1539d94f8ec21
Valdas Stonkus <mr.valdas.stonkus@gmail.com>
2019-03-16 20:55:26 +0200

    Added dels data to global Vuex store

--- src/components/Kanban.vue
+++ src/components/Kanban.vue
@@ -17,89 +17,18 @@
 <script>
 export default {
   name: "KanBan",
-  data() {
-    return {
-      stages: [
-        "Įvesta",
-        "Bendraujama",
-        "Reikalingas pasiūlymas",
-        "Pasiūlymas pateiktas",
-        "Užsakymas vykdomas"
-      ],
-      deals: [
-        {
-          id: 0,
-          status: "Įvesta",
-          title: "Buy coffee machine"
-        },
-        {
-          id: 1,
-          status: "Įvesta",
-          title: "Buy coffee machine"
-        },
-        {
-          id: 2,
-          status: "Įvesta",
-          title: "Buy coffee machine"
-        },
-        {
-          id: 3,
-          status: "Įvesta",
-          title: "Buy coffee machine"
-        },
-        {
-          id: 4,
-          status: "Įvesta",
-          title: "Find better AirBnB options"
-        },
-        {
-          id: 5,
-          status: "Įvesta",
-          title: "Find"
-        },
-        {
-          id: 6,
-          status: "Įvesta",
-          title: "Find better AirBnB options sdfsdfds sdfsdf"
-        },
-        {
-          id: 7,
-          status: "Įvesta",
-          title: "Buy coffee machine"
-        },
-        {
-          id: 8,
-          status: "Įvesta",
-          title: "Buy coffee machine"
-        },
-        {
-          id: 9,
-          status: "Įvesta",
-          title: "Buy coffee machine"
-        },
-        {
-          id: 10,
-          status: "Įvesta",
-          title: "Buy coffee machine"
-        },
-        {
-          id: 11,
-          status: "Įvesta",
-          title: "Find better AirBnB options"
-        },
-        {
-          id: 12,
-          status: "Įvesta",
-          title: "Find"
-        },
-        {
-          id: 13,
-          status: "Užsakymas vykdomas",
-          title: "Find better AirBnB options sdfsdfds sdfsdf"
-        }
-      ]
-    };
+  //Get data of deals from global Vuex store
+  computed: {
+    stages() {
+      return this.$store.state.stages;
+    },
+    deals() {
+      return this.$store.state.deals;
+    }
   },
+  // data() {
+  //   return {};
+  // },
   methods: {
     updateDeal(id, status) {
       this.deals.find(d => d.id === Number(id)).status = status;
--- src/store.js
+++ src/store.js
@@ -1,10 +1,92 @@
+//This Vuex store will be used for global access of data to any component
 import Vue from "vue";
 import Vuex from "vuex";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    //Set data for Kanban
+    stages: [
+      "Įvesta",
+      "Bendraujama",
+      "Reikalingas pasiūlymas",
+      "Pasiūlymas pateiktas",
+      "Užsakymas vykdomas"
+    ],
+    deals: [
+      {
+        id: 0,
+        status: "Įvesta",
+        title: "Buy coffee machine"
+      },
+      {
+        id: 1,
+        status: "Įvesta",
+        title: "Buy coffee machine"
+      },
+      {
+        id: 2,
+        status: "Įvesta",
+        title: "Buy coffee machine"
+      },
+      {
+        id: 3,
+        status: "Įvesta",
+        title: "Buy coffee machine"
+      },
+      {
+        id: 4,
+        status: "Įvesta",
+        title: "Find better AirBnB options"
+      },
+      {
+        id: 5,
+        status: "Įvesta",
+        title: "Find"
+      },
+      {
+        id: 6,
+        status: "Įvesta",
+        title: "Find better AirBnB options sdfsdfds sdfsdf"
+      },
+      {
+        id: 7,
+        status: "Įvesta",
+        title: "Buy coffee machine"
+      },
+      {
+        id: 8,
+        status: "Įvesta",
+        title: "Buy coffee machine"
+      },
+      {
+        id: 9,
+        status: "Įvesta",
+        title: "Buy coffee machine"
+      },
+      {
+        id: 10,
+        status: "Įvesta",
+        title: "Buy coffee machine"
+      },
+      {
+        id: 11,
+        status: "Įvesta",
+        title: "Find better AirBnB options"
+      },
+      {
+        id: 12,
+        status: "Įvesta",
+        title: "Find"
+      },
+      {
+        id: 13,
+        status: "Užsakymas vykdomas",
+        title: "Find better AirBnB options sdfsdfds sdfsdf"
+      }
+    ]
+  },
   mutations: {},
   actions: {}
 });
