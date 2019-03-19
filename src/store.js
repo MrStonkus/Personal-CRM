6187c174e22d7c3c81b83aa6bae5c6edf3da6d20
Valdas Stonkus <mr.valdas.stonkus@gmail.com>
2019-03-19 22:20:44 +0200

    Updated fake store deals

--- src/store.js
+++ src/store.js
@@ -19,32 +19,86 @@ export default new Vuex.Store({
       {
         id: "sdffgfvbngsfsf",
         status: "Įvesta",
-        company: "UAB Kompanija2",
+        company: "UAB Praeityje tikslus",
         contact: "Tomas Baltušis",
         activityDate: "2016-03-17T13:23:00.957Z",
         allDayActivity: false,
         product: "Vyniotuvas",
         action: "Laukiu kol griš iš atostogų"
       },
+
       {
         id: "sdfasdesrtetresfgfsfsf",
         status: "Įvesta",
-        company: "UAB Flirtas",
+        company: "Aktyvumas nenustatytas",
         contact: "Rokas",
-        activityDate: "2015-03-17T16:00:00.957Z",
-        allDayActivity: true,
+        activityDate: "",
+        allDayActivity: false,
         product: "Stelažas",
         action: "Neapsisprendžia"
       },
       {
-        id: "kgyjfghruouiouifg",
+        id: "ererouifg",
         status: "Įvesta",
-        company: "UAB Softas",
+        company: "Šiandien visa diena",
         // contact: "Tadas Kernagis",
-        activityDate: "2019-03-17T17:15:00.957Z",
+        activityDate: "2019-03-19T17:15:00.957Z",
+        allDayActivity: true,
+        product: "Vyniotuvas",
+        action: "Paruošti KP"
+      },
+
+      {
+        id: "kgyjfghr8998uifg",
+        status: "Įvesta",
+        company: "Šiandien tikslus ateityje",
+        contact: "Tadas Kernagis",
+        activityDate: "2019-03-19T21:15:00.957Z",
         allDayActivity: false,
         product: "Vyniotuvas",
         action: "Paruošti KP"
+      },
+      {
+        id: "2434hr8998uifg",
+        status: "Įvesta",
+        company: "Ateityje tikslus",
+        contact: "Tadas Kernagis",
+        activityDate: "2020-03-19T21:15:00.957Z",
+        allDayActivity: false,
+        product: "Vyniotuvas",
+        action: "Paruošti KP"
+      },
+      {
+        id: "kgyjf3243sd998uifg",
+        status: "Įvesta",
+        company: "Ateityje visa diena",
+        contact: "Tadas Kernagis",
+        activityDate: "2021-03-19T21:15:00.957Z",
+        allDayActivity: true,
+        product: "Vyniotuvas",
+        action: "Paruošti KP"
+      },
+
+      {
+        id: "kgyjfghruouiouifg",
+        status: "Įvesta",
+        company: "Šiandien tikslus pavėluotas",
+        contact: "Tadas Kernagis",
+        activityDate: "2019-03-19T10:15:00.957Z",
+        allDayActivity: false,
+        product: "Vyniotuvas",
+        action: "Paruošti KP"
+      },
+
+      {
+        id: "sdffgf55vbngsfsf",
+        status: "Įvesta",
+        company: "Praeityje visa diena",
+        contact: "Tomas Baltušis",
+        activityDate: "2017-03-17T13:23:00.957Z",
+        allDayActivity: true,
+        product: "Vyniotuvas",
+        action: "Laukiu kol griš iš atostogų"
       }
     ],
     //Default global settings - not activated, just for future example!
